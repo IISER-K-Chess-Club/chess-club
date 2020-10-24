41b82cc6761363af9c0e7f1b8380c05cea8392df
Satvik Saha <sahasatvik@gmail.com>
2020-10-24 11:44:55 +0530

    Removed all form related code

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-$( ".required" ).on('input', function() {
-    if ($(this).val() != "") {
-        $(this).parent().find(".required_label").hide();
-    }
-    else {
-        $(this).parent().find(".required_label").show();
-    }
-});
-
 $(document).ready(function() {
 
     new WOW().init();
@@ -25,16 +16,8 @@ $(document).ready(function() {
         }
     };
 
-    $( "#sign_up_button" ).on('click', function() {
-        scrollTo("signup");
-    });
-
     $( "a.scrollable" ).on('click', function(event) {
         scrollTo($(this).attr("href").substring(1));
-    });
-
-    $( "#go_back" ).on('click', function() {
-        document.location.href= "/";
     });
 
     $('.carousel').flickity({
